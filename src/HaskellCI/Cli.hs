7180878e3720b0412626f1988d03a3ad3b1440b8
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-02-11 22:41:59 +0200

    Better handling of options

--- src/HaskellCI/Cli.hs
+++ src/HaskellCI/Cli.hs
@@ -11,7 +11,7 @@ import           System.IO                (hPutStrLn, stderr)
 import qualified Options.Applicative      as O
 
 import           HaskellCI.Config
-import           HaskellCI.OptionsGrammar
+import           HaskellCI.OptparseGrammar
 
 -------------------------------------------------------------------------------
 -- Command
@@ -55,7 +55,7 @@ optionsP = Options
     <$> pure []
     <*> O.optional (O.strOption (O.long "output" <> O.short 'o' <> O.metavar "FILE" <> O.help "Optput file (stdout if omitted)"))
     <*> O.optional (O.strOption (O.long "config" <> O.metavar "CONFIGFILE" <> O.help "Configuration file"))
-    <*> runOptionsGrammar configGrammar
+    <*> runOptparseGrammar configGrammar
 
 cliParserInfo :: O.ParserInfo (Command, Options)
 cliParserInfo = O.info ((,) <$> cmdP <*> optionsP O.<**> O.helper) $ mconcat
---
+++ src/HaskellCI/OptparseGrammar.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+module HaskellCI.OptparseGrammar (
+    OptparseGrammar,
+    runOptparseGrammar,
+    ) where
+
+import           Prelude                     ()
+import           Prelude.Compat
+
+import           Control.Applicative         (many)
+import           Data.Foldable               (asum)
+import           Distribution.Simple.Utils   (fromUTF8BS)
+
+import qualified Distribution.Compat.Lens    as C
+import qualified Distribution.Compat.Newtype as C
+import qualified Distribution.FieldGrammar   as C
+import qualified Distribution.Parsec.Class   as C
+import qualified Distribution.Parsec.Field   as C
+import qualified Distribution.Pretty         as C
+import qualified Options.Applicative         as O
+
+import           HaskellCI.OptionsGrammar
+
+data SomeParser s where
+    SP :: (Maybe String -> Maybe String -> O.Parser (s -> s)) -> SomeParser s
+
+newtype OptparseGrammar s a = OG [SomeParser s]
+  deriving Functor
+
+runOptparseGrammar :: OptparseGrammar s a -> O.Parser (s -> s)
+runOptparseGrammar (OG ps) = fmap (foldr (flip (.)) id) $ many $ asum
+    [ p Nothing Nothing
+    | SP p <- ps
+    ]
+
+instance Applicative (OptparseGrammar s) where
+    pure _ = OG []
+    OG f <*> OG x = OG (f ++ x)
+
+instance C.FieldGrammar OptparseGrammar where
+    blurFieldGrammar l (OG ps) = OG
+        [ SP $ \v h -> fmap (l C.#%~) (p v h)
+        | SP p <- ps
+        ]
+
+    -- we don't support unique fields atm
+    uniqueFieldAla _ _ _ = OG []
+
+    -- the non default flag has help entry
+    booleanFieldDef fn l def = OG
+        [ SP $ \_m h -> setOG l $ O.flag' True  $ flagMods fn (th h)
+        , SP $ \_m h -> setOG l $ O.flag' False $ flagMods ("no-" <> fn) (fh h)
+        ]
+      where
+        th h = if def then Nothing else h
+        fh h = if def then h else Nothing
+
+    optionalFieldAla fn c l = OG
+        [ SP $ \m h -> setOptionalOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m h ]
+
+    optionalFieldDefAla fn c l def = OG
+        [ SP $ \m h -> setOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m (fmap hdef h) ]
+      where
+        hdef h = h ++ " (Default: " ++ C.prettyShow (C.pack' c def) ++ ")"
+
+    monoidalFieldAla fn c l = OG
+        [ SP $ \m h -> monoidOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m h ]
+
+    prefixedFields _ _   = pure []
+    knownField _         = pure ()
+    deprecatedSince _  _ = id
+    availableSince _ _   = id
+    hiddenField          = id
+
+instance OptionsGrammar OptparseGrammar where
+    help h (OG ps) = OG
+        [ SP $ \m _h -> p m (Just h)
+        | SP p <- ps
+        ]
+
+    metahelp m h (OG ps) = OG
+        [ SP $ \_m _h -> p (Just m) (Just h)
+        | SP p <- ps
+        ]
+
+optionMods :: (O.HasName mods, O.HasMetavar mods) => C.FieldName -> Maybe String -> Maybe String -> O.Mod mods a
+optionMods fn mmetavar mhelp = flagMods fn mhelp
+    <> maybe mempty O.metavar mmetavar
+
+flagMods :: O.HasName mods => C.FieldName -> Maybe String -> O.Mod mods a
+flagMods fn mhelp = O.long (fromUTF8BS fn)
+    <> maybe mempty O.help mhelp
+
+readMParsec :: C.Parsec a => O.ReadM a
+readMParsec = O.eitherReader C.eitherParsec
+
+setOG :: C.ALens' s a -> O.Parser a -> O.Parser (s -> s)
+setOG l = fmap (l C.#~)
+
+setOptionalOG :: C.ALens' s (Maybe a) -> O.Parser a -> O.Parser (s -> s)
+setOptionalOG l = fmap $ \x -> l C.#~ Just x
+
+monoidOG :: Monoid a => C.ALens' s a -> O.Parser a -> O.Parser (s -> s)
+monoidOG l = fmap $ \x -> l C.#%~ \y -> y <> x
