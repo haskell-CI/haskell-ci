5301a471effa984a27e426d2360bf3933c2b2b10
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-02-24 09:39:30 +0100

    Fixup #488: use `completer`/`bashCompletere` instead of `action`
    Suggested in @phadej's review.

--- src/HaskellCI/OptparseGrammar.hs
+++ src/HaskellCI/OptparseGrammar.hs
@@ -25,7 +25,7 @@ import qualified Options.Applicative         as O
 import HaskellCI.OptionsGrammar
 
 data SomeParser s where
-    SP :: (Maybe MetaVar -> Maybe BashCompletionAction -> Maybe Help -> O.Parser (s -> s)) -> SomeParser s
+    SP :: (Maybe MetaVar -> Maybe O.Completer -> Maybe Help -> O.Parser (s -> s)) -> SomeParser s
 
 newtype OptparseGrammar s a = OG [SomeParser s]
   deriving Functor
@@ -42,7 +42,7 @@ instance Applicative (OptparseGrammar s) where
 
 instance C.FieldGrammar ParsecPretty OptparseGrammar where
     blurFieldGrammar l (OG ps) = OG
-        [ SP $ \v a h -> fmap (l C.#%~) (p v a h)
+        [ SP $ \v c h -> fmap (l C.#%~) (p v c h)
         | SP p <- ps
         ]
 
@@ -51,23 +51,23 @@ instance C.FieldGrammar ParsecPretty OptparseGrammar where
 
     -- the non default flag has help entry
     booleanFieldDef fn l def = OG
-        [ SP $ \_m _a h -> setOG l $ O.flag' True  $ flagMods fn (th h)
-        , SP $ \_m _a h -> setOG l $ O.flag' False $ flagMods ("no-" <> fn) (fh h)
+        [ SP $ \_m _c h -> setOG l $ O.flag' True  $ flagMods fn (th h)
+        , SP $ \_m _c h -> setOG l $ O.flag' False $ flagMods ("no-" <> fn) (fh h)
         ]
       where
         th h = if def then Nothing else h
         fh h = if def then h else Nothing
 
     optionalFieldAla fn c l = OG
-        [ SP $ \m a h -> setOptionalOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m a h ]
+        [ SP $ \m cpl h -> setOptionalOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m cpl h ]
 
     optionalFieldDefAla fn c l def = OG
-        [ SP $ \m a h -> setOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m a (fmap hdef h) ]
+        [ SP $ \m cpl h -> setOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m cpl (fmap hdef h) ]
       where
         hdef h = h ++ " (Default: " ++ C.prettyShow (C.pack' c def) ++ ")"
 
     monoidalFieldAla fn c l = OG
-        [ SP $ \m a h -> monoidOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m a h ]
+        [ SP $ \m cpl h -> monoidOG l $ O.option (C.unpack' c <$> readMParsec) $ optionMods fn m cpl h ]
 
     prefixedFields _ _   = pure []
     knownField _         = pure ()
@@ -77,27 +77,27 @@ instance C.FieldGrammar ParsecPretty OptparseGrammar where
     hiddenField          = id
 
     freeTextField fn l = OG
-        [ SP $ \m a h -> setOptionalOG l $ O.strOption $ optionMods fn m a h ]
+        [ SP $ \m c h -> setOptionalOG l $ O.strOption $ optionMods fn m c h ]
 
     freeTextFieldDef fn l = OG
-        [ SP $ \m a h -> setOG l $ O.strOption $ optionMods fn m a h ]
+        [ SP $ \m c h -> setOG l $ O.strOption $ optionMods fn m c h ]
 
     freeTextFieldDefST fn l = OG
-        [ SP $ \m a h -> setOG l $ O.strOption $ optionMods fn m a h ]
+        [ SP $ \m c h -> setOG l $ O.strOption $ optionMods fn m c h ]
 
 instance OptionsGrammar ParsecPretty OptparseGrammar where
     help h (OG ps) = OG
-        [ SP $ \m a _h -> p m a (Just h)
+        [ SP $ \m c _h -> p m c (Just h)
         | SP p <- ps
         ]
 
     metahelp m h (OG ps) = OG
-        [ SP $ \_m a _h -> p (Just m) a (Just h)
+        [ SP $ \_m c _h -> p (Just m) c (Just h)
         | SP p <- ps
         ]
 
     metaActionHelp m a h (OG ps) = OG
-        [ SP $ \_m _a _h -> p (Just m) (Just a) (Just h)
+        [ SP $ \_m _c _h -> p (Just m) (Just $ O.bashCompleter a) (Just h)
         | SP p <- ps
         ]
 
@@ -110,19 +110,19 @@ instance OptionsGrammar ParsecPretty OptparseGrammar where
     -- where the --no-tests has help, because it's not default.
     --
     rangeField fn l def = OG
-        [ SP $ \_m _a  h -> setOG l $ O.flag' C.anyVersion $ flagMods fn (th h)
-        , SP $ \_m _a  h -> setOG l $ O.flag' C.noVersion  $ flagMods ("no-" <> fn) (fh h)
-        , SP $ \_m _a _h -> setOG l $ O.option readMParsec $ O.long (fromUTF8BS $ fn <> "-jobs") <> O.metavar "RANGE"
+        [ SP $ \_m _c  h -> setOG l $ O.flag' C.anyVersion $ flagMods fn (th h)
+        , SP $ \_m _c  h -> setOG l $ O.flag' C.noVersion  $ flagMods ("no-" <> fn) (fh h)
+        , SP $ \_m _c _h -> setOG l $ O.option readMParsec $ O.long (fromUTF8BS $ fn <> "-jobs") <> O.metavar "RANGE"
         ]
       where
         th h = if equivVersionRanges def C.anyVersion then Nothing else h
         fh h = if equivVersionRanges def C.anyVersion then h else Nothing
 
 optionMods :: (O.HasName mods, O.HasCompleter mods, O.HasMetavar mods)
-           => C.FieldName -> Maybe MetaVar -> Maybe BashCompletionAction -> Maybe Help -> O.Mod mods a
-optionMods fn mmetavar mact mhelp = flagMods fn mhelp
+           => C.FieldName -> Maybe MetaVar -> Maybe O.Completer -> Maybe Help -> O.Mod mods a
+optionMods fn mmetavar mcompl mhelp = flagMods fn mhelp
     <> maybe mempty O.metavar mmetavar
-    <> maybe mempty O.action  mact
+    <> maybe mempty O.completer mcompl
 
 flagMods :: O.HasName mods => C.FieldName -> Maybe Help -> O.Mod mods a
 flagMods fn mhelp = O.long (fromUTF8BS fn)
