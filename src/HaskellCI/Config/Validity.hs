55347c739e7a9ae6ba0a021326494ded503d70c1
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-04-11 14:30:01 +0300

    Rename ShError into HsCiError

--- src/HaskellCI/Config/Validity.hs
+++ src/HaskellCI/Config/Validity.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards  #-}
 module HaskellCI.Config.Validity where
 
 import HaskellCI.Prelude
@@ -7,14 +7,14 @@ import HaskellCI.Prelude
 import HaskellCI.Compiler
 import HaskellCI.Config
 import HaskellCI.Config.Ubuntu
+import HaskellCI.Error
 import HaskellCI.Jobs
 import HaskellCI.MonadErr
-import HaskellCI.Sh
 
 -- Validity checks shared in common among all backends.
-checkConfigValidity :: MonadErr ShError m => Config -> JobVersions -> m ()
+checkConfigValidity :: MonadErr HsCiError m => Config -> JobVersions -> m ()
 checkConfigValidity Config {..} JobVersions {..} = do
     when (anyGHCJS && cfgUbuntu > Bionic) $
-        throwErr $ ShError $ "Using GHCJS requires Ubuntu 16.04 (Xenial) or 18.04 (Bionic)."
+        throwErr $ ValidationError $ "Using GHCJS requires Ubuntu 16.04 (Xenial) or 18.04 (Bionic)."
   where
     anyGHCJS = any isGHCJS versions
