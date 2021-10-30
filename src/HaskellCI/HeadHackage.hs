1cf5351ae91ba62ec15e3f266ebdaaa0db83893b
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-10-30 05:52:46 +0300

    head.hackage with ghc >=9.3

--- src/HaskellCI/HeadHackage.hs
+++ src/HaskellCI/HeadHackage.hs
@@ -5,7 +5,7 @@ import HaskellCI.Prelude
 import qualified Distribution.Version as C
 
 defaultHeadHackage :: VersionRange
-defaultHeadHackage = C.orLaterVersion (C.mkVersion [9,1])
+defaultHeadHackage = C.orLaterVersion (C.mkVersion [9,3])
 
 headHackageRepoStanza :: [String]
 headHackageRepoStanza =
