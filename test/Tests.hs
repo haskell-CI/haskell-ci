167b600721da46371d0d2e344716ebcadd715c38
Francesco Gazzetta <fgaz@fgaz.me>
2021-11-29 20:56:31 +0100

    sourcehut: add --parallel test

--- test/Tests.hs
+++ test/Tests.hs
@@ -32,6 +32,7 @@ main = do
         , fixtureGoldenTest "psql"
         , fixtureGoldenTest "travis-patch"
         , fixtureGoldenTest "enabled-jobs"
+        , fixtureGoldenTest "sourcehut-parallel"
         , testGroup "copy-fields"
             [ fixtureGoldenTest "copy-fields-all"
             , fixtureGoldenTest "copy-fields-some"
