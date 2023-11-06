ec6af35eabb420e5b70373719ffb00f30c70a306
Oleg Grenrus <oleg.grenrus@iki.fi>
2023-11-06 12:47:58 +0200

    Update cabal-install to 3.10.2.0

--- src/HaskellCI/Cabal.hs
+++ src/HaskellCI/Cabal.hs
@@ -10,7 +10,7 @@ defaultCabalInstallVersion = Just (C.mkVersion [3,10])
 -- | Convert cabal-install version to a version ghcup understands.
 cabalGhcupVersion :: Version -> Version
 cabalGhcupVersion ver = case C.versionNumbers ver of
-      [3,10] -> C.mkVersion [3,10,1,0]
+      [3,10] -> C.mkVersion [3,10,2,0]
       [3,9]  -> C.mkVersion [3,9,0,0]
       [3,6]  -> C.mkVersion [3,6,2,0]
       [x,y]  -> C.mkVersion [x,y,0,0]
