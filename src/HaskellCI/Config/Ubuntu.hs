a36d6fa232c8edd5e27833573c6345a08cb484de
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-05-01 15:19:53 +0300

    Drop GHC-7 support, Ubuntu Bionic&Xenial; Add Noble Numbat
    Resolves #719

--- src/HaskellCI/Config/Ubuntu.hs
+++ src/HaskellCI/Config/Ubuntu.hs
@@ -6,24 +6,22 @@ import qualified Distribution.Parsec as C
 import qualified Distribution.Pretty as C
 import qualified Text.PrettyPrint    as PP
 
-data Ubuntu = Xenial | Bionic | Focal | Jammy
+data Ubuntu = Focal | Jammy | Noble
   deriving (Eq, Ord, Show, Enum, Bounded)
 
 instance C.Parsec Ubuntu where
     parsec = do
         t <- C.parsecToken
         case t of
-            "xenial" -> return Xenial
-            "bionic" -> return Bionic
             "focal"  -> return Focal
             "jammy"  -> return Jammy
+            "noble"  -> return Noble
             _        -> fail $ "Unknown ubuntu release " ++ t
 
 instance C.Pretty Ubuntu where
     pretty = PP.text . showUbuntu
 
 showUbuntu :: Ubuntu -> String
-showUbuntu Xenial = "xenial"
-showUbuntu Bionic = "bionic"
 showUbuntu Focal  = "focal"
 showUbuntu Jammy  = "jammy"
+showUbuntu Noble  = "noble"
