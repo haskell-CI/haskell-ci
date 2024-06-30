0996c05a448e48bc3a4ce2dc81eb958f6b7f8387
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-06-30 20:43:11 +0200

    Default to cabal-3.12.1.0

--- src/HaskellCI/Cabal.hs
+++ src/HaskellCI/Cabal.hs
@@ -5,11 +5,12 @@ import HaskellCI.Prelude
 import qualified Distribution.Version as C
 
 defaultCabalInstallVersion :: Maybe Version
-defaultCabalInstallVersion = Just (C.mkVersion [3,10])
+defaultCabalInstallVersion = Just (C.mkVersion [3,12])
 
 -- | Convert cabal-install version to a version ghcup understands.
 cabalGhcupVersion :: Version -> Version
 cabalGhcupVersion ver = case C.versionNumbers ver of
+      [3,12] -> C.mkVersion [3,12,1,0]
       [3,10] -> C.mkVersion [3,10,2,0]
       [3,9]  -> C.mkVersion [3,9,0,0]
       [3,6]  -> C.mkVersion [3,6,2,0]
