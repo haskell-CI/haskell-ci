cf7d83319105ece5cb61aba73260f380f1da079c
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-07-01 20:46:16 +0300

    Make GHCUP the only installation method for cabal-install
    Also remove version mapping, Closes #732

--- src/HaskellCI/Cabal.hs
+++ src/HaskellCI/Cabal.hs
@@ -1,14 +1,1 @@
 module HaskellCI.Cabal where
-
-import HaskellCI.Prelude
-
-import qualified Distribution.Version as C
-
--- | Convert cabal-install version to a version ghcup understands.
-cabalGhcupVersion :: Version -> Version
-cabalGhcupVersion ver = case C.versionNumbers ver of
-      [3,10] -> C.mkVersion [3,10,2,0]
-      [3,9]  -> C.mkVersion [3,9,0,0]
-      [3,6]  -> C.mkVersion [3,6,2,0]
-      [x,y]  -> C.mkVersion [x,y,0,0]
-      _      -> ver
