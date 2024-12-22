cd844d5b4102b8aebd764577d3bc2bf4efa78b7b
Ryan Scott <rscott@galois.com>
2024-12-22 10:13:44 -0600

    head.hackage>=9.13
    See https://github.com/haskell-CI/haskell-ci/issues/725#issuecomment-2558505396

--- src/HaskellCI/HeadHackage.hs
+++ src/HaskellCI/HeadHackage.hs
@@ -5,7 +5,7 @@ import HaskellCI.Prelude
 import qualified Distribution.Version as C
 
 defaultHeadHackage :: VersionRange
-defaultHeadHackage = C.orLaterVersion (C.mkVersion [9,11])
+defaultHeadHackage = C.orLaterVersion (C.mkVersion [9,13])
 
 headHackageRepoStanza :: Bool -> [String]
 headHackageRepoStanza override =
