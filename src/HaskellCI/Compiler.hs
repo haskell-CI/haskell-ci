355b9a90e0ecdaba31c72ddc1b0e5de7a59bb7a4
Oleg Grenrus <oleg.grenrus@iki.fi>
2025-08-21 18:13:59 +0300

    Add GHC-9.14 alpha1

--- src/HaskellCI/Compiler.hs
+++ src/HaskellCI/Compiler.hs
@@ -146,6 +146,7 @@ knownGhcVersions = fmap mkVersion
     , [9,8,1],  [9,8,2],  [9,8,3], [9,8,4]
     , [9,10,1], [9,10,2]
     , [9,12,1], [9,12,2]
+    , [9,14,1]
     ]
 
 knownGhcjsVersions :: [Version]
